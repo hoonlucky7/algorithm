8745df03ce943a42bcb8b32fce63ab3c02fdf866
hoonlucky7 <55186501+hoonlucky7@users.noreply.github.com>
2025-01-17 21:12:47 +0900

    Update PrimeChecker.java

--- basic/prime/PrimeChecker.java
+++ basic/prime/PrimeChecker.java
@@ -2,6 +2,7 @@ public class PrimeChecker {
 
     public static boolean isPrime(int number) {
         if (number <= 1) return false;
+        if (number == 2) return true;
         for (int i = 2; i <= Math.sqrt(number); i++) {
             if (number % i == 0) return false;
         }
