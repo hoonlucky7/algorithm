fe3cff46d8315431e21bf5cd94c5f0f1c0492f51
hoonlucky7 <55186501+hoonlucky7@users.noreply.github.com>
2025-01-17 21:17:56 +0900

    Update PrimeChecker.py

--- basic/prime/PrimeChecker.py
+++ basic/prime/PrimeChecker.py
@@ -3,6 +3,8 @@ import math
 def is_prime(number):
     if number <= 1:
         return False
+    if (number == 1:
+        return True
     for i in range(2, int(math.sqrt(number)) + 1):
         if number % i == 0:
             return False
